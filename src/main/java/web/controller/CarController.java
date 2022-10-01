4192b02b815d62d7f82e41d70bb9a1141f766a2e
DmitriySolomatin <solomatin@megavega.ru>
2022-10-01 18:08:01 +0400

    fix cars.html

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,2 +1,28 @@
-package web.controller;public class CarController {
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.service.CarService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+
+@Controller
+public class CarController {
+    @Autowired
+    private CarService carService;
+
+    @GetMapping(value = "/cars")
+    public String printCars(ModelMap model, @RequestParam("count") Optional<Integer> count) {
+        List<Car> cars = carService.getCarsList(count.orElse(5));
+        model.addAttribute("cars", cars);
+        return "cars";
+    }
+
 }
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,2 +1,24 @@
-package web.service;public class CarServiceImp {
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImp implements CarService{
+    private static final List<Car> carsList = new ArrayList<>();
+    static {
+        carsList.add(new Car("Tesla", "X", 1000));
+        carsList.add(new Car("Tesla", "S", 2000));
+        carsList.add(new Car("Tesla", "Y", 3000));
+        carsList.add(new Car("Tesla", "3", 4000));
+        carsList.add(new Car("Tesla", "CYBERTRUCK", 5000));
+    }
+
+    @Override
+    public List<Car> getCarsList(int count) {
+        return carsList.subList(0, Math.min(count, 5));
+    }
 }
